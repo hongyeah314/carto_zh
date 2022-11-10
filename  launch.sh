5db06c8254cbcac6b299ce5ddff44697bb484ab7
zhanglei <1158204108@qq.com>
2022-11-10 09:17:20 +0800

    update

---
+++  launch.sh
@@ -0,0 +1,2 @@
+source devel/setup.bash
+roslaunch cartographer_ros demo_backpack_2d.launch bag_filename:=/home/zhanglei/bagfiles/b0-2014-07-11-10-58-16.bag
---
+++ make.sh
@@ -0,0 +1,4 @@
+mkdir build
+cd build
+cmake .. -DCATKIN_DEVEL_PREFIX=../devel
+make
